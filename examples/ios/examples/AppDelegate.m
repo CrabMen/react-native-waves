5dd128730684c7f493b71b5fbe1ce800a470db85
郭强 <guoqiang@guoqiangdeMacBook-Pro.local>
2019-11-04 11:49:06 +0800

    init 0.59.0 rn project and run the project

--- examples/ios/examples/AppDelegate.m
+++ examples/ios/examples/AppDelegate.m
@@ -34,6 +34,7 @@
 {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
